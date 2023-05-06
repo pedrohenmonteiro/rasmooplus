db11df4b2500fedc5d7eca11e607dd8ac3666e78
Pedro Monteiro <pedromont436@gmail.com>
2023-05-05 21:15:30 -0300

    first commit

---
+++ src/main/java/com/mont/rasmooplus/RasmooplusApplication.java
@@ -0,0 +1,13 @@
+package com.mont.rasmooplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasmooplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RasmooplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mont/rasmooplus/RasmooplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mont.rasmooplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RasmooplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
